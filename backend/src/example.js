897ae148022aa61da30bb84ad6e9d8b7e3904471
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-09-10 00:31:15 +0000

    build: updated example with actual post

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,2 +1,13 @@
 import { initDatabase } from './db/init.js'
-initDatabase()
+import { Post } from './db/models/post.js'
+
+await initDatabase()
+
+const post = new Post({
+  title: 'Hello Mongoose!',
+  author: 'Huseyin Ergin',
+  contents: 'This post is stored in a MongoDF database using Mongoose.',
+  tags: ['mongoose', 'mongodb'],
+})
+
+await post.save()
