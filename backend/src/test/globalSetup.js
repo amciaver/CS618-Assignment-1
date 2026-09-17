b755c7c96bd8f6055d9457a73905a33be3cbb28a
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-09-17 16:37:02 +0000

    test: fixed typo

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -2,9 +2,9 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 export default async function globalSetup() {
   const instance = await MongoMemoryServer.create({
     binary: {
-      version: '8.0.10',
+      version: '6.0.4',
     },
   })
-  global._MONGOINSTANCE = instance
+  global.__MONGOINSTANCE = instance
   process.env.DATABASE_URL = instance.getUri()
 }
