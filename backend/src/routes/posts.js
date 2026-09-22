b6477ba8dff00be5d62aa0842cb560f15043c145
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-09-22 23:37:29 +0000

    build: added more routes

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -2,9 +2,11 @@ import {
   listAllPosts,
   listPostsByAuthor,
   listPostsByTag,
+  createPost,
+  updatePost,
+  deletePost,
   getPostById,
 } from '../services/posts.js'
-
 export function postsRoutes(app) {
   app.get('/api/v1/posts', async (req, res) => {
     const { sortBy, sortOrder, author, tag } = req.query
@@ -34,6 +36,34 @@ export function postsRoutes(app) {
       return res.json(post)
     } catch (err) {
       console.error('error getting post', err)
+      return res.status(500).end()
+    }
+  })
+  app.post('/api/v1/posts', async (req, res) => {
+    try {
+      const post = await createPost(req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error creating post', err)
+      return res.status(500).end()
+    }
+  })
+  app.patch('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const post = await updatePost(req.params.id, req.body)
+      return res.json(post)
+    } catch (err) {
+      console.error('error updating post', err)
+      return res.status(500).end()
+    }
+  })
+  app.delete('/api/v1/posts/:id', async (req, res) => {
+    try {
+      const { deletedCount } = await deletePost(req.params.id)
+      if (deletedCount === 0) return res.sendStatus(404)
+      return res.status(204).end()
+    } catch (err) {
+      console.error('error deleting post', err)
       return res.status(500).end()
     }
   })
