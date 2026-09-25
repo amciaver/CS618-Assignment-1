1f8728732940824f9ed5dcd1530567da1a89de63
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-09-25 14:21:39 +0000

    build: updated app to include postlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Post } from './components/Post.jsx'
+import { PostList } from './components/PostList.jsx'
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
 export function App() {
-  return (
-    <Post
-      title='Full-Stack React Projects'
-      contents="Let's become full-stack developers!"
-      author='Daniel Bugl'
-    />
-  )
+  return <PostList posts={posts} />
 }
